f37ec99a53301ab8a37538ebee167b07fdcce3a6
katarinaducic <katarina.ducic@elfak.rs>
2026-08-07 03:36:55 +0200

    promena tabele Napredak u bazi

---
+++ Gradjevinska_firma/Gradjevinska_firma/Entiteti/Napredak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Entiteti
+{
+    public class Napredak
+    {   
+        public virtual int Id { get; protected set; }
+        public virtual DateTime Datum { get; set; }
+        public virtual Zadatak Zadatak { get; set; }
+        public virtual string DnevniIzvestaj { get; set; }
+        public virtual int ProcenatRealizacije { get; set; }
+        public virtual string PrimedbaNadzora { get; set; }
+        public virtual string KorektivnaMera { get; set; }
+        public virtual IList<Fotografija> Fotografije { get; set; }
+
+        public Napredak()
+        {
+            Fotografije = new List<Fotografija>();
+        }
+    }
+}
---
+++ Gradjevinska_firma/Gradjevinska_firma/Mapiranja/NapredakMap.cs
@@ -0,0 +1,36 @@
+﻿using FluentNHibernate.Mapping;
+using Gradjevinska_firma.Entiteti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Mapiranja
+{
+    public class NapredakMap:ClassMap<Napredak>
+    {
+        public NapredakMap()
+        {
+            Table("NAPREDAK");
+
+            Id(x => x.Id, "ID")
+                .GeneratedBy.TriggerIdentity();
+
+            Map(x => x.Datum, "DATUM");
+            Map(x => x.DnevniIzvestaj, "DNEVNI_IZVESTAJ");
+            Map(x => x.ProcenatRealizacije, "PROCENAT_REALIZACIJE");
+            Map(x => x.PrimedbaNadzora, "PRIMEDBA_NADZORA");
+            Map(x => x.KorektivnaMera, "KOREKTIVNA_MERA");
+
+            References(x => x.Zadatak)
+                .Column("IDZADATAK");
+
+            HasMany(x => x.Fotografije)
+                .KeyColumn("IDNAPREDAK")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+        }
+    }
+}
