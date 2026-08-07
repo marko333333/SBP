6276447d482e822525e56267dfbe4a50bb5431eb
katarinaducic <katarina.ducic@elfak.rs>
2026-08-07 03:38:01 +0200

    promena tabele Faza u bazi

---
+++ Gradjevinska_firma/Gradjevinska_firma/Entiteti/Faza.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Entiteti
+{
+    public class Faza
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string Naziv { get; set; }
+        public virtual string TipFaze { get; set; }
+        public virtual DateTime? DatumOd { get; set; }
+        public virtual DateTime? DatumDo { get; set; }
+        public virtual string Status { get; set; }
+        public virtual int? Budzet { get; set; }
+        public virtual Projekat Projekat { get; set; }
+        public virtual FizickoLice FizickoLice { get; set; }
+        public virtual Faza NadFaza { get; set; }
+
+        public virtual IList<Faza> PodFaze { get; set; }
+
+        public virtual IList<Zadatak> Zadaci { get; set; }
+
+        public Faza()
+        {
+            PodFaze = new List<Faza>();
+            Zadaci = new List<Zadatak>();
+        }
+    }
+}
---
+++ Gradjevinska_firma/Gradjevinska_firma/Mapiranja/FazaMap.cs
@@ -0,0 +1,49 @@
+﻿using FluentNHibernate.Mapping;
+using Gradjevinska_firma.Entiteti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Mapiranja
+{
+    public class FazaMap:ClassMap<Faza>
+    {
+        public FazaMap()
+        {
+            Table("FAZA");
+
+            Id(x => x.Id, "ID")
+                .GeneratedBy.TriggerIdentity();
+
+            Map(x => x.Naziv, "NAZIV");
+            Map(x => x.TipFaze, "TIP_FAZE");
+            Map(x => x.DatumOd, "DATUM_OD");
+            Map(x => x.DatumDo, "DATUM_DO");
+            Map(x => x.Status, "STATUS");
+            Map(x => x.Budzet, "BUDZET");
+
+            References(x => x.Projekat)
+                .Column("IDPROJEKTA");
+
+            References(x => x.FizickoLice)
+                .Column("IDFIZICKO_LICE");
+
+            References(x => x.NadFaza)
+                .Column("ID_NADFAZE");
+
+            HasMany(x => x.PodFaze)
+                .KeyColumn("ID_NADFAZE")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+
+            HasMany(x => x.Zadaci)
+                .KeyColumn("IDFAZA")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+        }
+    }
+}
