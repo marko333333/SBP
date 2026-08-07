e1c5e0f8708b4f26d31854053966a3ed92d00d13
katarinaducic <katarina.ducic@elfak.rs>
2026-08-07 03:35:47 +0200

    promena tabele fotografija u bazi

---
+++ Gradjevinska_firma/Gradjevinska_firma/Entiteti/Fotografija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Entiteti
+{
+    public class Fotografija
+    {
+        public virtual Napredak Napredak { get; set; }
+        public virtual string Putanja { get; set; }
+    }
+}
---
+++ Gradjevinska_firma/Gradjevinska_firma/Mapiranja/FotografijaMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using Gradjevinska_firma.Entiteti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Mapiranja
+{
+    public class FotografijaMap:ClassMap<Fotografija>
+    {
+        public FotografijaMap()
+        {
+            Table("FOTOGRAFIJA");
+
+            CompositeId()
+                .KeyReference(x => x.Napredak,"IDNAPREDAK")
+                .KeyProperty(x => x.Putanja, "FOTOGRAFIJA");
+        }
+    }
+}
