36b82344845a0c5eaa4d905d52e9db7df1e3473a
katarinaducic <katarina.ducic@elfak.rs>
2026-08-07 03:39:17 +0200

    promena tabele Zadatak u bazi

---
+++ Gradjevinska_firma/Gradjevinska_firma/Entiteti/Zadatak.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Entiteti
+{
+    public class Zadatak
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string Naziv { get; set; }
+        public virtual string Opis { get; set; }
+        public virtual decimal ProcenjeniTrosak { get; set; }
+        public virtual DateTime? PlaniraniZavrsetak { get; set; }
+        public virtual DateTime? StvarniZavrsetak { get; set; }
+        public virtual DateTime? PlaniraniPocetak { get; set; }
+        public virtual DateTime? StvarniPocetak { get; set; }
+        public virtual int Prioritet { get; set; }
+        public virtual string Status { get; set; }
+        public virtual Faza Faza { get; set; }
+        public virtual Zadatak Roditelj { get; set; }
+
+        public virtual IList<Zadatak> Podzadaci { get; set; }
+        public virtual IList<RadniNalog> RadniNalozi { get; set; }
+        public virtual IList<Napredak> Napreci { get; set; }
+        public virtual IList<KontrolaKvaliteta> KontroleKvaliteta { get; set; }
+        public virtual IList<Angazovan> Angazovani { get; set; }
+        public virtual IList<Angazuje> AngazovanaOprema { get; set; }
+
+
+        public Zadatak() {
+
+            Podzadaci = new List<Zadatak>();
+            RadniNalozi = new List<RadniNalog>();
+            Napreci = new List<Napredak>();
+            KontroleKvaliteta = new List<KontrolaKvaliteta>();
+            Angazovani = new List<Angazovan>();
+            AngazovanaOprema = new List<Angazuje>();
+        }
+    }
+}
---
+++ Gradjevinska_firma/Gradjevinska_firma/Mapiranja/ZadatakMap.cs
@@ -0,0 +1,72 @@
+﻿using FluentNHibernate.Mapping;
+using Gradjevinska_firma.Entiteti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradjevinska_firma.Mapiranja
+{
+    public class ZadatakMap:ClassMap<Zadatak>
+    {   
+        public ZadatakMap() {
+
+            Table("ZADATAK");
+
+            Id(x => x.Id, "ID")
+                .GeneratedBy.TriggerIdentity();
+            Map(x => x.Naziv, "NAZIV");
+            Map(x => x.Opis, "OPIS");
+            Map(x => x.ProcenjeniTrosak, "PROCENJENI_TROOSAK");
+            Map(x => x.PlaniraniZavrsetak, "PLANIRANI_ZAVRSETAK");
+            Map(x => x.StvarniZavrsetak, "STVARNI_ZAVRSETAK");
+            Map(x => x.PlaniraniPocetak, "PLANIRANI_POCETAK");
+            Map(x => x.StvarniPocetak, "STVARNI_POCETAK");
+            Map(x => x.Prioritet, "PRIORITET");
+            Map(x => x.Status, "STATUS");
+
+            References(x => x.Faza)
+                .Column("ID_FAZE");
+
+            References(x => x.Roditelj)
+                .Column("ID_ZADATAK_RODITELJ");
+
+            HasMany(x => x.Podzadaci)
+                .KeyColumn("ID_ZADATAK_RODITELJ")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+
+            HasMany(x => x.RadniNalozi)
+                .KeyColumn("IDZADATAK")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+
+            HasMany(x => x.Napreci)
+                .KeyColumn("IDZADATAK")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+
+            HasMany(x => x.KontroleKvaliteta)
+                .KeyColumn("IDZADATKA")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+
+            HasMany(x => x.Angazovani)
+                .KeyColumn("IDZADATAK")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+
+            HasMany(x => x.AngazovanaOprema)
+                .KeyColumn("IDZADATAK")
+                .LazyLoad()
+                .Cascade.All()
+                .Inverse();
+        }
+    }
+}
