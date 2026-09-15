39a856e31af5db0279b9789e0a95547c219957f4
Marko <markobarbulovic058@gmail.com>
2026-09-15 18:38:57 +0200

    BezbednosniIncident

--- Gradjevinska_firma_WebAPI/Gradjevinska_firmaLibrary/DataProvider/BezbednosniIncidentDataProvider.cs
+++ Gradjevinska_firma_WebAPI/Gradjevinska_firmaLibrary/DataProvider/BezbednosniIncidentDataProvider.cs
@@ -43,5 +43,236 @@ namespace Gradjevinska_firmaLibrary.DataProvider
 
             return data;
         }
+
+        public static async Task<Result<BezbednosniIncidentView, ErrorMessage>> DodajBezbednosniIncidentAsync(BezbednosniIncidentView f)
+        {
+            ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+
+                if (!(s?.IsConnected ?? false))
+                    return "Nemoguce otvoriti sesiju.".ToError(403);
+
+                Projekat? projekat = await s.QueryOver<Projekat>()
+                    .Where(x => x.ID == f.ProjekatID)
+                    .SingleOrDefaultAsync();
+
+                if (projekat == null)
+                    return "Projekat ne postoji.".ToError(404);
+
+                Osoba? osoba = await s.QueryOver<Osoba>().Where(x=>x.Id == f.OsobaID).SingleOrDefaultAsync();
+
+                if (osoba == null)
+                    return "Osoba ne postoji.".ToError(404);
+
+                BezbednosniIncident incident;
+                switch (f.Tip_incidenta)
+                {
+                    case "Povreda na radu":
+                        incident = new PovredaNaRadu();
+                        break;
+
+                    case "Kvar opreme":
+                        incident = new KvarOpreme();
+                        break;
+
+                    case "Nepostovanje procedura":
+                        incident = new NepostovanjeProcedura();
+                        break;
+
+                    case "Opasna situacija":
+                        incident = new OpasnaSituacija();
+                        break;
+
+                    case "Ekoloski incident":
+                        incident = new EkoloskiIncident();
+                        break;
+
+                    default:
+                        return "Nepoznat tip incidenta.".ToError(400);
+                }
+
+                incident.Opis = f.Opis;
+                incident.Datum = f.Datum;
+                incident.Lokacija = f.Lokacija;
+                incident.Preduzete_mere = f.Preduzete_mere;
+                incident.Posledice = f.Posledice;
+                incident.Tip_incidenta = f.Tip_incidenta;
+                incident.Projekat = projekat;
+                incident.Osoba = osoba;
+
+
+
+                await s.SaveOrUpdateAsync(incident);
+                await s.FlushAsync();
+
+                return new BezbednosniIncidentView(incident);
+            }
+            catch (Exception)
+            {
+                return "Doslo je do greske prilikom dodavanja incidenta.".ToError(400);
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task<Result<BezbednosniIncidentView, ErrorMessage>> vratiBezbednosniIncidentAsync(int id)
+        {
+            ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+
+                if (!(s?.IsConnected ?? false))
+                    return "Nemoguce otvoriti sesiju.".ToError(403);
+
+                BezbednosniIncident? incident = await s.QueryOver<BezbednosniIncident>()
+                    .Where(x => x.ID == id)
+                    .SingleOrDefaultAsync();
+
+                if (incident == null)
+                    return "Incident ne postoji.".ToError(404);
+
+                return new BezbednosniIncidentView(incident);
+            }
+            catch (Exception)
+            {
+                return "Doslo je do greske prilikom dobavljanja bezbednosnog incidenta.".ToError(400);
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task<Result<BezbednosniIncidentView, ErrorMessage>> izmeniBezbednosniIncidentAsync(BezbednosniIncidentView f)
+        {
+            ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+
+                if (!(s?.IsConnected ?? false))
+                    return "Nemoguce otvoriti sesiju.".ToError(403);
+
+                BezbednosniIncident? incident = await s.QueryOver<BezbednosniIncident>()
+                    .Where(x => x.ID == f.ID)
+                    .SingleOrDefaultAsync();
+
+                if (incident == null)
+                    return "Faktura ne postoji.".ToError(404);
+
+                Projekat? projekat = await s.QueryOver<Projekat>()
+                    .Where(x => x.ID == f.ProjekatID)
+                    .SingleOrDefaultAsync();
+
+                if (projekat == null)
+                    return "Projekat ne postoji.".ToError(404);
+
+                Osoba? osoba = await s.QueryOver<Osoba>().Where(x=>x.Id == f.OsobaID).SingleOrDefaultAsync();
+                if (osoba == null)
+                    return "Osoba ne postoji.".ToError(404);
+
+                incident.Opis = f.Opis;
+                incident.Datum = f.Datum;
+                incident.Lokacija = f.Lokacija;
+                incident.Preduzete_mere = f.Preduzete_mere;
+                incident.Posledice = f.Posledice;
+                incident.Tip_incidenta = f.Tip_incidenta;
+                incident.Projekat = projekat;
+                incident.Osoba = osoba;
+
+                await s.UpdateAsync(incident);
+                await s.FlushAsync();
+
+                return new BezbednosniIncidentView(incident);
+            }
+            catch (Exception)
+            {
+                return "Doslo je do greske prilikom izmene fakture.".ToError(400);
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task<Result<bool, ErrorMessage>> ObrisiBezbednosniIncidentAsync(int id)
+        {
+            ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+
+                if (!(s?.IsConnected ?? false))
+                    return "Nemoguce otvoriti sesiju.".ToError(403);
+
+                BezbednosniIncident? incident = await s.QueryOver<BezbednosniIncident>()
+                    .Where(x => x.ID == id)
+                    .SingleOrDefaultAsync();
+
+                if (incident == null)
+                    return "Incident ne postoji.".ToError(404);
+
+                await s.DeleteAsync(incident);
+                await s.FlushAsync();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return "Doslo je do greske prilikom brisanja incidenta.".ToError(400);
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task<Result<List<BezbednosniIncidentView>, ErrorMessage>> VratiBezbednosneIncidenteProjektaAsync(int projekatId)
+        {
+            ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+
+                if (!(s?.IsConnected ?? false))
+                    return "Nemoguce otvoriti sesiju.".ToError(403);
+
+                List<BezbednosniIncidentView> data =
+                    (await s.QueryOver<BezbednosniIncident>()
+                        .Where(x => x.Projekat.ID == projekatId)
+                        .ListAsync())
+                    .Select(x => new BezbednosniIncidentView(x))
+                    .ToList();
+
+                if (data.Count == 0)
+                    return "Projekat nema incidente.".ToError(404);
+
+                return data;
+            }
+            catch (Exception)
+            {
+                return "Doslo je do greske prilikom dobijanja incidenta projekta.".ToError(400);
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
     }
 }
--- Gradjevinska_firma_WebAPI/Gradjevinska_firmaWebAPI/Controllers/BezbednosniIncidentController.cs
+++ Gradjevinska_firma_WebAPI/Gradjevinska_firmaWebAPI/Controllers/BezbednosniIncidentController.cs
@@ -24,5 +24,86 @@ namespace Gradjevinska_firmaWebAPI.Controllers
 
             return Ok(result.Data);
         }
+
+        [HttpPost]
+        [Route("DodajBezbednosniIncident")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DodajBezbednosniIncident([FromBody] BezbednosniIncidentView f)
+        {
+            var result = await BezbednosniIncidentDataProvider.DodajBezbednosniIncidentAsync(f);
+
+            if (result.IsError)
+                return StatusCode(result.Error.StatusCode, result.Error.Message);
+
+            return Ok(result.Data);
+        }
+
+        [HttpGet]
+        [Route("VratiBezbednosniIncident/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> vratiBezbednosniIncident(int id)
+        {
+            var result = await BezbednosniIncidentDataProvider.vratiBezbednosniIncidentAsync(id);
+
+            if (result.IsError)
+                return StatusCode(result.Error.StatusCode, result.Error.Message);
+
+            return Ok(result.Data);
+        }
+
+        [HttpPut]
+        [Route("IzmeniBezbednosniIncident")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> izmeniBezbednosniIncident([FromBody] BezbednosniIncidentView f)
+        {
+            var result = await BezbednosniIncidentDataProvider.izmeniBezbednosniIncidentAsync(f);
+
+            if (result.IsError)
+                return StatusCode(result.Error.StatusCode, result.Error.Message);
+
+            return Ok(result.Data);
+        }
+
+        [HttpDelete]
+        [Route("ObrisiBezbednosniIncident/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> obrisiBezbednosniIncident(int id)
+        {
+            var result = await BezbednosniIncidentDataProvider.ObrisiBezbednosniIncidentAsync(id);
+
+            if (result.IsError)
+                return StatusCode(result.Error.StatusCode, result.Error.Message);
+
+            return Ok(result.Data);
+        }
+
+        [HttpGet]
+        [Route("VratiBezbednosneIncidenteProjekta/{idProjekat}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> VratiBezbednosneIncidenteProjekta(int idProjekat)
+        {
+            var result = await BezbednosniIncidentDataProvider.VratiBezbednosneIncidenteProjektaAsync(idProjekat);
+
+            if (result.IsError)
+                return StatusCode(result.Error.StatusCode, result.Error.Message);
+
+            return Ok(result.Data);
+        }
+
     }
 }
