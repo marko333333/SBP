b1e0d07a753f1a7917ae7fcf3ce5b0733ab3b1e8
katarinaducic <katarina.ducic@elfak.rs>
2026-09-24 16:00:02 +0200

    dodate ?

--- Gradjevinska_firma/Gradjevinska_firma/Entiteti/Koristi.cs
+++ Gradjevinska_firma/Gradjevinska_firma/Entiteti/Koristi.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace Gradjevinska_firma.Entiteti
 {
-    public class Koristi//klasa za M:N zadatak-materijal
+    public class Koristi
     {
-        //ja sam stavljala da negde bude surogat, tipa kod napredak sada imamo da ima Id, i jos negde
-        public virtual int ID { get; protected set; }//surogat kljuc ili mora kompozitni? ZA SVAKU M:N SAM KORISTIO SUROGAT
+        public virtual int ID { get; protected set; }
         public virtual Zadatak Zadatak { get; set; }
         public virtual Materijal Materijal { get; set; }
         public virtual int Kolicina { get; set; }
